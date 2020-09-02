700b6473be0cf1f5030fecddc260a45411306b2d
viettoan1998 <toanpro98@gmail.com>
2020-09-02 11:08:05 +0700

    toan 2/9

--- app/src/main/java/com/qlct/mymoney/fragment/CreateFianceFragment.java
+++ app/src/main/java/com/qlct/mymoney/fragment/CreateFianceFragment.java
@@ -5,15 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.qlct.mymoney.R;
 
 public class CreateFianceFragment extends Fragment {
+
+    private Toolbar toolbar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_fiance, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        toolbar = view.findViewById(R.id.toolbarMain);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((AppCompatActivity)getActivity()).onBackPressed();
+            }
+        });
     }
 }
