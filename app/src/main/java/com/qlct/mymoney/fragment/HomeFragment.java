2ae57722ee0792b071165bb90bcb1dd018e1e09e
huy <elpulga97@gmail.com>
2020-09-07 15:46:07 +0700

    wallet ui

--- app/src/main/java/com/qlct/mymoney/fragment/HomeFragment.java
+++ app/src/main/java/com/qlct/mymoney/fragment/HomeFragment.java
@@ -5,25 +5,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.qlct.mymoney.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private View view;
 
     public HomeFragment() {
-        // Required empty public constructor
-    }
 
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        CollapsingToolbarLayout collapsingToolbar = view.findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("Total: 2,000,000$");
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        iniViews();
+    }
+
+    private void iniViews(){
+        recyclerView = view.findViewById(R.id.recycleView);
+    }
+
+
 }
