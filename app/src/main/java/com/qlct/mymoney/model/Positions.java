e533d6d4be82b3b613bb864c63097bdacd0f17d6
duongkibo <kibzz0zzz@gmail.com>
2020-12-06 22:57:24 +0700

    fix update

---
+++ app/src/main/java/com/qlct/mymoney/model/Positions.java
@@ -0,0 +1,34 @@
+package com.qlct.mymoney.model;
+
+public class Position {
+    double x;
+    double y;
+
+    public Position() {
+    }
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Position(double x) {
+        this.x = x;
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
